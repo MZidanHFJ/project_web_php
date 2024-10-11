b0b8f7937145167c4496c0975beb4d8cad383cbe
Zidan <zidanm4505@gmail.com>
2024-10-11 13:48:02 +0700

    first commit

---
+++ pages/aboutme.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tentang Saya</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <div style="text-align: center;">
+        <h2>Tentang Saya</h2>
+        <img src="zidan.jpg" alt="foto saya" style="width: 10rem; border: 1px solid black">
+        <br></br>
+        <table class="table table-striped" style="margin: 0 auto; width: auto;">
+        <tr>
+            <th style="text-align: left;">Nama</th>
+            <td style="text-align: left;">Muhammad Zidan Hikayatuloh</td>
+        </tr>
+        <tr>
+            <th style="text-align: left;">Status</th>
+            <td style="text-align: left;">Pelajar</td>
+        </tr>
+        <tr>
+            <th style="text-align: left;">Sekolah</th>
+            <td style="text-align: left;">SMKN 4 Padalarang</td>
+        </tr>
+        <tr>
+            <th style="text-align: left;">Tanggal Lahir</th>
+            <td style="text-align: left;">07 Juni 2004</td>
+        </tr>
+        <tr>
+            <th style="text-align: left;">Hobi</th>
+            <td style="text-align: left;">Bermain Game</td>
+        </tr>
+        <tr>
+            <th style="text-align: left;">Makanan favorit</th>
+            <td style="text-align: left;">Sate</td>
+        </tr>
+        </table>
+        <br></br>
+        <a href="../index.php" ><button type="button" class="btn btn-primary">Kembali</button></a>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB>">
+    
+</body>
+</html>
